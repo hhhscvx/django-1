9a2b2b276887540577487ec9d49d295c40d04909
hhhscvx <hhhscvx@yandex.ru>
2023-12-10 17:36:38 +0500

    add comments

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,10 +4,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
-    list_filter = ['status', 'created', 'publish', 'author']
-    search_fields = ['title', 'body']
+    list_display = ['title', 'slug', 'author', 'publish', 'status']  # элементы сортировки
+    list_filter = ['status', 'created', 'publish', 'author']  # справа filter
+    search_fields = ['title', 'body']  # значения, по которым можно осуществлять поиск
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
+    ordering = ['status', 'publish']  # сортировка по умолчанию

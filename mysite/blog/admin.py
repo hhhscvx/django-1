dc64d7d53dc96fe9a5351ef1b7d878224367f279
hhhscvx <hhhscvx@yandex.ru>
2023-12-11 08:53:38 +0500

    Создали представление для поиска и отображения Постов

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'author', 'publish', 'status']  # элементы сортировки
     list_filter = ['status', 'created', 'publish', 'author']  # справа filter
     search_fields = ['title', 'body']  # значения, по которым можно осуществлять поиск
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ['author']
+    prepopulated_fields = {'slug': ('title',)}  # при создании поста slug автоматически заполняется как title
+    raw_id_fields = ['author']  # при создании поста добавляется поиск авторов для упрощения жизни
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']  # сортировка по умолчанию

5e3fc7e2a45cdc2a72363a145fc17205b05cf5ac
hhhscvx <hhhscvx@yandex.ru>
2023-12-12 12:34:20 +0500

    Добавлено представление на основе класса

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger  # Pag позволяет осуществлять постраничную разбивку результатов, дальше импортируем ошибки
+from django.views.generic import ListView
 
 
 def post_list(request):
@@ -30,3 +31,11 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+
+class PostListView(ListView):
+    """Альтернативное представление списка постов"""
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'

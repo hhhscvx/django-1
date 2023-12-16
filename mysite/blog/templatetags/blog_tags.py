4ce1cb0aa4ebfc43b3ee12735e2003e37e1572c9
hhhscvx <hhhscvx@yandex.ru>
2023-12-16 08:32:55 +0500

    можно создавать посты через markdown в server/admin

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -21,3 +23,8 @@ def get_most_commented_posts(count=3):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).exclude(total_comments=0).order_by('-total_comments')[:count]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):  # Принимает text в формате markdown
+    return mark_safe(markdown.markdown(text))  # С помощью markdown преобразует в HTML -> помечается безопасным файлом через mark_safe хуй знает нахуя

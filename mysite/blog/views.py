421b82d5ea5acc450bff218b29cd40cbe0479140
hhhscvx <hhhscvx@yandex.ru>
2023-12-16 20:05:07 +0500

    project is Done..)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_list(request, tag_slug=None):
@@ -114,9 +114,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)  # ранг это типо насколько соответсвует/похоже на запрос
+            ).filter(search=search_query).order_by('-rank')  # сортируем по похожести, по рангу
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
